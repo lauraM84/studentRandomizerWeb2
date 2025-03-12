6359764ff74e79dfdec3d060a0514c1c7d5b8a9b
labstraction <labstraction@protonmail.com>
2025-03-12 12:42:48 +0100

    finale

--- src/app.js
+++ src/app.js
@@ -2,56 +2,99 @@ import DataService from "./services/data-service.js";
 
 const service = new DataService();
 
-const studentData = service.getShuffledStudents()
-
-const container = document.getElementById('students-container');
-
-for (let i = 0; i < studentData.length; i++) {
-
-    const student = studentData[i];
-
-    const studentContainer = document.createElement('div');
-
-    studentContainer.classList.add('student-container')
-
-    const nameContainer = document.createElement('h3');
-
-    // nameContainer.style.color = "red";
-
-    const nameNode = document.createTextNode(student.name + ' ' + student.surname)
-
-    nameContainer.appendChild(nameNode)
-
-    const countryContainer = document.createElement('span');
-
-    const countryNode = document.createTextNode('Nazionalità: ' + student.nationality);
-
-    countryContainer.appendChild(countryNode);
-
-    const genderContainer = document.createElement('span');
-
-    const genderNode = document.createTextNode('Genere: ' + student.gender);
-
-    genderContainer.appendChild(genderNode);
-
-    const ageContainer = document.createElement('span');
-
-    const ageNode = document.createTextNode('Età: ' + student.getAge());
-
-    ageContainer.appendChild(ageNode);
-
-    studentContainer.appendChild(nameContainer);
-
-    studentContainer.appendChild(countryContainer);
-
-    studentContainer.appendChild(genderContainer);
-
-    studentContainer.appendChild(ageContainer);
-
-    container.appendChild(studentContainer);
-    
+function orderByName(){
+    const studentData = service.getStudentsByName()
+    reder(studentData);
 }
 
+function orderByAge(){
+    const studentData = service.getStudentsByAge()
+    reder(studentData);
+}
+
+function shuffle(){
+    const studentData = service.getShuffledStudents()
+    reder(studentData);
+}
+
+window.orderByName = orderByName;
+window.orderByAge = orderByAge;
+window.shuffle = shuffle;
+
+function reder(studentData){
+    const container = document.getElementById('students-container');
+
+    container.innerHTML = '';
+
+    for (let i = 0; i < studentData.length; i++) {
+    
+        const student = studentData[i];
+    
+        const studentContainer = document.createElement('div');
+    
+        studentContainer.classList.add('student-container')
+
+        const nameContainer = createTextElement('h3', student.name + ' ' + student.surname)
+    
+        // const nameContainer = document.createElement('h3');
+    
+        // // nameContainer.style.color = "red";
+    
+        // const nameNode = document.createTextNode(student.name + ' ' + student.surname)
+    
+        // nameContainer.appendChild(nameNode)
+
+        const countryContainer = createTextElement('span', 'Nazionalità: ' + student.nationality)
+    
+        // const countryContainer = document.createElement('span');
+    
+        // const countryNode = document.createTextNode('Nazionalità: ' + student.nationality);
+    
+        // countryContainer.appendChild(countryNode);
+
+        const genderContainer = createTextElement('span', 'Genere: ' + student.gender)
+    
+        // const genderContainer = document.createElement('span');
+    
+        // const genderNode = document.createTextNode('Genere: ' + student.gender);
+    
+        // genderContainer.appendChild(genderNode);
+
+        const ageContainer = createTextElement('span', 'Età: ' + student.getAge())
+    
+        // const ageContainer = document.createElement('span');
+    
+        // const ageNode = document.createTextNode('Età: ' + student.getAge());
+    
+        // ageContainer.appendChild(ageNode);
+    
+        studentContainer.appendChild(nameContainer);
+    
+        studentContainer.appendChild(countryContainer);
+    
+        studentContainer.appendChild(genderContainer);
+    
+        studentContainer.appendChild(ageContainer);
+    
+        container.appendChild(studentContainer);
+        
+    }
+
+
+}
+
+
+function createTextElement(elementType, text) {
+
+    const element = document.createElement(elementType);
+
+    const node = document.createTextNode(text);
+
+    element.appendChild(node);
+
+    return element;
+        
+}
 
 
 
